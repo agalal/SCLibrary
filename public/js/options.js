7f853f2362cdb14cfd2fbc6072fd26aa48d7b97f
Milad Imen <miladmaaan@gmail.com>
2016-08-22 21:53:11 -0700

    added autoplay, created settings file, and moved waveform to new file

---
+++ public/js/options.js
@@ -0,0 +1,46 @@
+let _options = {
+  shuffle: false,
+  repeat: false,
+  autoplay: true,
+  autocheck: true
+}
+
+function setOpt(option, value){
+  _options[option] = value;
+}
+
+function toggleOpt(option){
+  _options[option] = !_options[option];
+}
+
+function getOpt(option){
+  return _options[option];
+}
+
+// function toggleLib() {
+//   if (!toggledLib) $('body').addClass('toggled');
+//   else $('body').removeClass('toggled');
+// }
+//
+// function toggleSet() {
+//   if (!toggledSet) $('#settings').addClass('toggled');
+//   else $('#settings').removeClass('toggled');
+// }
+
+function toggleAuto() {
+  if (!getOpt('autoplay')) $('#autoplay-toggle').addClass('toggled');
+  else $('#autoplay-toggle').removeClass('toggled');
+  toggleOpt('autoplay');
+}
+
+function toggleShuffle() {
+  if (!getOpt('shuffle')) $('#shuffle-toggle').addClass('toggled');
+  else $('#shuffle-toggle').removeClass('toggled');
+  toggleOpt('shuffle');
+}
+
+function toggleAutocheck() {
+  if (!getOpt('autocheck')) $('#autocheck-toggle').addClass('toggled');
+  else $('#autocheck-toggle').removeClass('toggled');
+  toggleOpt('autocheck');
+}
--- public/js/player/player.js
+++ public/js/player/player.js
@@ -1,12 +1,3 @@
-const options = {
-  refresh_rate: 24,
-  wf_percent: 97,
-  bar_width: 75,
-  bar_height: 0.5,
-  bar_y_offset: 1.5,
-  height: .3
-};
-
 var map = [];
 document.body.onkeydown = document.body.onkeyup = function(e) {
   e = e || event; // to deal with IE
@@ -68,14 +59,6 @@ function rewind(seconds) {
   audioPlayer.currentTime = audioPlayer.currentTime - seconds;
 }
 
-let window_width;
-
-function setWidth() {
-  window_width = Math.round($(window).width() / options.bar_width);
-}
-$(document).ready(setWidth);
-window.addEventListener("resize", setWidth);
-
 $('#artworkimg').click(playPause);
 
 //TODO: Seeking should be based on the width of the waveform, not the window's width.
@@ -93,15 +76,14 @@ $('#back-div').click(function(e) {
 let audioPlayer = new Audio();
 //Just did this cause the other guy did it, seems like its kew
 audioPlayer.crossOrigin = "anonymous";
-audioPlayer.addEventListener("ended", nextSong);
+audioPlayer.addEventListener("ended", autoplayNextSong);
 
-let audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-let audioSrc = audioCtx.createMediaElementSource(audioPlayer);
-let analyser = audioCtx.createAnalyser();
-let fd = new Uint8Array(256);
-
-audioSrc.connect(analyser);
-audioSrc.connect(audioCtx.destination);
+function autoplayNextSong(){
+  const autoplay = getOpt('autoplay');
+  if (autoplay) {
+    nextSong();
+  }
+}
 
 function loadSong(track) {
   var trackid = track.t.properties.scid;
@@ -137,111 +119,6 @@ function loadSong(track) {
   bgScroll(true, 0, seconds); // start bg scrolling
 }
 
-var refresh = false;
-var wform_data = [];
-
-function loadWaveform(track_id) {
-  d3.json("/api/tracks/" + track_id + "/waveform", function(error, data) {
-    if (error) throw error;
-    wform_data = data;
-    refresh = true;
-  });
-}
-
-// Repeatedly update the waveform in the player.
-setInterval(function() {
-  if (refresh) waveform();
-}, options.refresh_rate);
-
-var sub, lows, mids1, mids2, highs1, highs2;
-
-function composite(a, b, c, d, e, f, g) {
-  var num = (a * kick) + (b * lows) + (c * mids2) + (d * mids1) + (e * highs3) + (f * highs2) + (g * highs1);
-  var dem = a + b + c + d + e + f + g;
-  return num / dem;
-}
-
-function waveform() {
-  analyser.getByteFrequencyData(fd);
-  document.getElementById('wf_box').innerHTML = "";
-
-  highs1 = d3.mean(fd.slice(201, 246)) * options.height;
-  highs2 = d3.mean(fd.slice(165, 190)) * options.height;
-  highs3 = d3.mean(fd.slice(135, 155)) * options.height;
-  mids1 = d3.mean(fd.slice(99, 102)) * options.height * .95;
-  mids2 = d3.mean(fd.slice(60, 63)) * options.height * .9;
-  lows = d3.mean(fd.slice(34, 37)) * options.height * .8;
-  kick = d3.mean(fd.slice(7, 9)) * options.height * .7;
-
-  var data = [];
-  var b = 33 - window_width;
-  for (var i = 1; i < wform_data.length / b; i++) {
-    var total = 0;
-    for (var j = 0; j < b; j++) {
-      total += wform_data[(i * b) + j];
-    }
-    if (Math.round(total / b))
-      data.push(Math.round(total / b));
-  }
-
-  var max = d3.max(data);
-  var w = (7 - 12 / window_width)
-  var h = max * 2 || 0;
-
-  var x = d3.scale.linear()
-    .domain([0, 1])
-    .range([0, w]);
-
-  var y = d3.scale.linear()
-    .domain([0, h])
-    .rangeRound([0, h]); //rangeRound is used for antialiasing
-
-  var chart = d3.select(".charts").append("svg")
-    .attr("class", "chart")
-    .attr("width", "" + options.wf_percent + "%")
-    .attr("style", "padding-left:" + (100 - options.wf_percent) + "%;")
-    .attr("viewBox", "0 0 " + Math.max(w * data.length, 0) + " " + Math.max(h, 0))
-    .attr("fill", "white");
-  //TODO: Make a color analyzer for album artwork so that we can use a pallette to color things in the player, like fill.
-
-  chart.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("x", function(d, i) {
-      var x_offset = x(i) - Math.max(w * max / 900 - 0.25, 0.1) / 2;
-      return x_offset;
-    })
-    .attr("y", function(d, i) {
-      var height = y(d * options.bar_height) / h;
-      if (i % 9 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
-      if (i % 9 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
-      if (i % 9 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
-      if (i % 9 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
-      if (i % 9 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
-      if (i % 9 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
-      if (i % 9 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
-      if (i % 9 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
-      if (i % 9 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
-      return h - Math.pow(Math.max(height, .01), 1.5);
-    })
-    .attr("width", function(d) {
-      var width = Math.max((w * max / 900 - 0.25), 0.1);
-      return width;
-    })
-    .attr("height", function(d, i) {
-      var height = y(d * options.bar_height) / h;
-      if (i % 9 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
-      if (i % 9 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
-      if (i % 9 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
-      if (i % 9 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
-      if (i % 9 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
-      if (i % 9 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
-      if (i % 9 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
-      if (i % 9 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
-      if (i % 9 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
-      return Math.pow(Math.max(height, .01), 1.5) + options.bar_y_offset;
-    });
-}
 
 var lastShift = 0.0;
 
@@ -439,34 +316,4 @@ function nextListener() {
     attributes: true,
     attributeFilter: ['style']
   });
-}
-
-/* TODO: MOVE THIS STUFF INTO ANGULAR */
-var toggledLib = false;
-
-function toggleLib() {
-  if (!toggledLib) $('body').addClass('toggled');
-  else $('body').removeClass('toggled');
-  toggledLib = !toggledLib;
-}
-var toggledSet = false;
-
-function toggleSet() {
-  if (!toggledSet) $('#settings').addClass('toggled');
-  else $('#settings').removeClass('toggled');
-  toggledSet = !toggledSet;
-}
-var toggledAuto = true;
-
-function toggleAuto() {
-  if (!toggledAuto) $('#autoplay-toggle').addClass('toggled');
-  else $('#autoplay-toggle').removeClass('toggled');
-  toggledAuto = !toggledAuto;
-}
-var toggledShuffle = false;
-
-function toggleShuffle() {
-  if (!toggledShuffle) $('#shuffle-toggle').addClass('toggled');
-  else $('#shuffle-toggle').removeClass('toggled');
-  toggledShuffle = !toggledShuffle;
 }
