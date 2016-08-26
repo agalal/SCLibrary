afc548d5ab73d15bf8392f9dca402683806b068b
Nicholas Chase <chasemcdizzle@gmail.com>
2016-08-26 14:32:10 -0700

    color palette

--- public/js/player/player.js
+++ public/js/player/player.js
@@ -104,9 +104,13 @@ function loadSong(track) {
   highlightRow(track);
 
   audioPlayer.src = 'http://api.soundcloud.com/tracks/' + trackid + '/stream' + '?client_id=a3629314a336fd5ed371ff0f3e46d4d0';
+  //console.log(audioPlayer.src);
   audioPlayer.load();
   audioPlayer.play();
   loadWaveform(track.t._id);
+  var colors = loadArtworkPalette(track.t._id);
+console.log("colors:");
+  console.log(colors);
 
   $(".track-title").text(track.t.properties.name);
   $(".track-channel").text(track.c.properties.name);
@@ -127,6 +131,164 @@ function loadSong(track) {
   bgScroll(true, 0, seconds); // start bg scrolling
 }
 
+var refresh = false;
+var wform_data = [];
+
+function loadWaveform(track_id) {
+  d3.json("/api/tracks/" + track_id + "/waveform", function(error, data) {
+    if (error) throw error;
+    wform_data = data;
+    refresh = true;
+  });
+}
+
+var color_palette = {};
+var palette_refresh = false;
+//Calls backend to extract the color palette from the artwork URL for this track, returns JSON
+//If there is an error it returns null, perhaps we should make it return the "default" color palette (aka the white one)
+//There are 6 colors but I don't think they're all GUARANTEED to exist, see : http://localhost:3000/api/tracks/382/palette
+//Where LightVibrant is missing, need to look a little more into this to see what is guaranteed and when
+function loadArtworkPalette(track_id){
+  var request = $.ajax({
+    url: "/api/tracks/" + track_id + "/palette",
+    method: "GET",
+    dataType: "json",
+    //async: false,
+    success: function(data){
+      console.log("data:");
+      console.log(data);
+      color_palette = data;
+      palette_refresh = true;
+    },
+    fail: function(){
+      color_palette = null;
+      palette_refresh = true;
+    },
+  });
+}
+
+// Repeatedly update the waveform in the player.
+setInterval(function() {
+  if (refresh) waveform();
+  if (palette_refresh) {
+    palette_refresh = false;
+    updateColorPalette();
+  }
+}, options.refresh_rate);
+
+var sub, lows, mids1, mids2, highs1, highs2;
+
+function composite(a, b, c, d, e, f, g) {
+  var num = (a * kick) + (b * lows) + (c * mids2) + (d * mids1) + (e * highs3) + (f * highs2) + (g * highs1);
+  var dem = a + b + c + d + e + f + g;
+  return num / dem;
+}
+
+function updateColorPalette(){
+  //TODO: someone make this actually do cool things, this is just a test example, some1 replace this
+  if(color_palette != null){
+    console.log("updateColorPalette()");
+    //Some colors aren't guaranteed to exist, not sure why need to look more into this TODO
+    if(color_palette.LightMuted != null){
+      var lightMuted = color_palette.LightMuted;
+      var lightMutedRgbString = 'rgb('+lightMuted.rgb[0]+','+lightMuted.rgb[1]+','+lightMuted.rgb[2]+')';
+      $('.track-title').css('color', lightMutedRgbString);
+      $('.track-channel').css('color', lightMutedRgbString);
+    }
+    if(color_palette.Vibrant != null){
+      var vibrant = color_palette.Vibrant;
+      var vibrantRgbString = 'rgba('+vibrant.rgb[0]+','+vibrant.rgb[1]+','+vibrant.rgb[2]+','+.4+')';
+      $('.track-title').css('background-color', vibrantRgbString);
+    }
+    if(color_palette.DarkVibrant != null){
+      var darkVibrant = color_palette.DarkVibrant;
+      var darkVibrantRgbString = 'rgba('+darkVibrant.rgb[0]+','+darkVibrant.rgb[1]+','+darkVibrant.rgb[2]+','+.4+')';
+      $('.track-channel').css('background-color', darkVibrantRgbString);
+    }
+  }
+}
+
+function waveform() {
+  analyser.getByteFrequencyData(fd);
+  document.getElementById('wf_box').innerHTML = "";
+
+  highs1 = d3.mean(fd.slice(201, 246)) * options.height;
+  highs2 = d3.mean(fd.slice(165, 190)) * options.height;
+  highs3 = d3.mean(fd.slice(135, 155)) * options.height;
+  mids1 = d3.mean(fd.slice(99, 102)) * options.height * .95;
+  mids2 = d3.mean(fd.slice(60, 63)) * options.height * .9;
+  lows = d3.mean(fd.slice(34, 37)) * options.height * .8;
+  kick = ((d3.mean(fd.slice(7, 9)) * options.height * .7) - 15) * 1.7;
+
+  var data = [];
+  var b = 33 - window_width;
+  for (var i = 1; i < wform_data.length / b; i++) {
+    var total = 0;
+    for (var j = 0; j < b; j++) {
+      total += wform_data[(i * b) + j];
+    }
+    if (Math.round(total / b))
+      data.push(Math.round(total / b));
+  }
+
+  var max = d3.max(data);
+  var w = (7 - 12 / window_width)
+  var h = max * 2;
+
+  var x = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, w]);
+
+  var y = d3.scale.linear()
+    .domain([0, h])
+    .rangeRound([0, h]); //rangeRound is used for antialiasing
+
+  var chart = d3.select(".charts").append("svg")
+    .attr("class", "chart")
+    .attr("width", "" + options.wf_percent + "%")
+    .attr("style", "padding-left:" + (100 - options.wf_percent) + "%;")
+    .attr("viewBox", "0 0 " + Math.max(w * data.length, 0) + " " + Math.max(h, 0))
+    .attr("fill", "white");
+  //TODO: Make a color analyzer for album artwork so that we can use a pallette to color things in the player, like fill.
+
+  chart.selectAll("rect")
+    .data(data)
+    .enter().append("rect")
+    .attr("x", function(d, i) {
+      var x_offset = x(i) - Math.max(w * max / 900 - 0.25, 0.1) / 2;
+      return x_offset;
+    })
+    .attr("y", function(d, i) {
+      var height = y(d * options.bar_height) / h;
+      if (i % 9 === 0) height *= composite(1, 0, 0, 0, 0, 0, 0);
+      if (i % 9 === 1) height *= composite(1, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 2) height *= composite(1, 4, 0, 0, 0, 0, 0);
+      if (i % 9 === 3) height *= composite(3, 2, 0, 0, 0, 0, 0);
+      if (i % 9 === 4) height *= composite(5, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 5) height *= composite(3, 2, 0, 0, 0, 0, 0);
+      if (i % 9 === 6) height *= composite(1, 4, 0, 0, 0, 0, 0);
+      if (i % 9 === 7) height *= composite(1, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 8) height *= composite(1, 0, 0, 0, 0, 0, 0);
+      return h - Math.pow(Math.max(height, .01), 1.5);
+    })
+    .attr("width", function(d) {
+      var width = Math.max((w * max / 900 - 0.25), 0.1);
+      return width;
+    })
+    .attr("height", function(d, i) {
+      var height = y(d * options.bar_height) / h;
+      if (i % 9 === 0) height *= composite(1, 0, 0, 0, 0, 0, 0);
+      if (i % 9 === 1) height *= composite(1, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 2) height *= composite(1, 4, 0, 0, 0, 0, 0);
+      if (i % 9 === 3) height *= composite(3, 2, 0, 0, 0, 0, 0);
+      if (i % 9 === 4) height *= composite(5, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 5) height *= composite(3, 2, 0, 0, 0, 0, 0);
+      if (i % 9 === 6) height *= composite(1, 4, 0, 0, 0, 0, 0);
+      if (i % 9 === 7) height *= composite(1, 1, 0, 0, 0, 0, 0);
+      if (i % 9 === 8) height *= composite(1, 0, 0, 0, 0, 0, 0);
+      return Math.pow(Math.max(height, .01), 1.5) + options.bar_y_offset;
+    });
+}
 
 var lastShift = 0.0;
 
--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,179 @@ var request = require('./auth/request');
 var login = require('./auth/login');
 var admin = require('./auth/admin');
 
+//For auto-finding songs
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var GoogleSearch = require('google-search');
+var $ = require('jQuery');
+
+/*var googleSearch = new GoogleSearch({
+  key: 'AIzaSyAs5VY0JX2-B9St_3wQEacyjRDFRZ_OHe4',
+  cx: '014713471355677183001:be58owrujy4'
+});*/
+
 router.use('/login/', login);
 router.use('/request/', request);
 router.use('/admin/', admin);
 
 /* GET index page. */
 router.get('/', ensureLoggedOut, ensureLoggedIn);
+
+router.get('/songfind', ensureLoggedIn, function(req, res, next){
+  //In order to get the actual URL of the mp3 you need to resolve it
+  var preUrl = "http://api.soundcloud.com/tracks/264571232/stream?client_id=a3629314a336fd5ed371ff0f3e46d4d0";
+  var resolvedUrlBuf = "";
+  var resolvedUrl = "";
+  http.get(preUrl, function(res) {
+    res.on('data', function(chunk) {
+      resolvedUrlBuf += chunk;
+      //console.log("chunk");
+      //console.log(chunk.toString());
+    });
+    res.on('end', function() {
+      // all data has been downloaded
+      var responseJson = JSON.parse(resolvedUrlBuf);
+      console.log("responseJson.location:");
+      console.log(responseJson.location);
+      resolvedUrl = responseJson.location;
+      getMp3FromResolvedUrl(resolvedUrl);
+      //getSearchResults
+      /*console.log(body);
+      console.log("finished downloading");*/
+    });
+  });
+
+});
+
+  function getMp3FromResolvedUrl(resolvedUrl){
+    //Now download the song
+    var songFile = fs.createWriteStream("test.mp3");
+    //var fileBuf = new Buffer();
+    https.get(resolvedUrl, function(res) {
+    res.on('data', function(chunk) {
+      songFile.write(chunk);
+    });
+    res.on('end', function() {
+      //whole song has been downloaded
+      songFile.end();
+      console.log("finished downloading song");
+
+      var songName = "Justin Martin & Ardalan Ft. PartyPatty- FUNCTION VIP";
+      getGoogleZippyResults(songName);
+      //getZippySearchResults(songName);
+      //customGoogleSearch(songName);
+      /*console.log(body);
+      console.log("finished downloading");*/
+    });
+  });
+  }
+
+  function getGoogleZippyResults(songName){
+    var searchUrl = "https://www.google.com/search?q=";
+    songName += " site:zippyshare.com"
+    console.log(parseSongNameWhitespace(songName));
+    searchUrl += parseSongNameWhitespace(songName);
+    //Get Results
+    var htmlString = "";
+    console.log("searchUrl: " + searchUrl);
+    https.get(searchUrl, function(res) {
+      res.on('data', function(chunk) {
+        htmlString += chunk;
+        //console.log("chunk");
+        //console.log(chunk.toString());
+      });
+      res.on('end', function() {
+        // all data has been downloaded
+        console.log("resolved zippysearch results:");
+        var regex = /<cite>(.*)<\/cite>/g;
+        var searchUrls = htmlString.match(regex);
+        for(var i = 0; i < searchUrls.length; i++){
+            searchUrls[i] = searchUrls[i].replace("<cite>", "");
+            searchUrls[i] = searchUrls[i].replace("</cite>", "");
+            console.log(searchUrls[i]);
+        }
+        downloadSearchResults(searchUrls, 0);
+        /*var string = '<div><input type="text" value="val" /></div>';*/
+        //$('<div/>').html(htmlString).contents();
+/*        var string = '<div><input type="text" value="val" /></div>',
+        object = $('<div/>').html(string).contents();
+        console.log(object.find('cite').val());
+        var html = html = $.parseHTML(htmlString);
+*/
+        //console.log(htmlString);
+      });
+    });
+  }
+
+//zippyshare wants me to use these or else it rejects my connection, hot.
+var agentOptions = {
+    host: 'zippyshare.com'
+//, port: '443'
+ ,path: '/'
+, rejectUnauthorized: false
+};
+
+var agent = new http.Agent(agentOptions);
+  //this might just be actually loading zippy's home page.
+  function downloadSearchResults(urls, index){
+    if(index < urls.length){
+      var domainString = urls[index].substring(0, urls[index].indexOf('.'));
+      console.log("domainString: " + domainString);
+      var htmlString = "";
+      console.log("going to url: " + urls[index]);
+      http.get({url: "http://" + urls[index], agent: agent}, function(res) {
+      //http.get({url: "http://" + urls[index]}, function(res) {
+      res.on('data', function(chunk) {
+        htmlString += chunk;
+        //console.log("chunk");
+        //console.log(chunk.toString());
+      });
+      res.on('end', function() {
+        // all data has been downloaded
+        console.log("resolved google results:");
+        console.log(htmlString);
+        var regex = /<a id=\"dlbutton\" href=\">(.*)\">/g;
+        var searchUrls = htmlString.match(regex);
+        for(var i = 0; i < searchUrls.length; i++){
+            searchUrls[i] = searchUrls[i].replace("<cite>", "");
+            searchUrls[i] = searchUrls[i].replace("</cite>", "");
+            console.log(searchUrls[i]);
+        }
+        //var regex = /<cite>(.*)<\/cite>/g;
+      });
+    });
+    }
+  }
+
+  function getZippySearchResults(songName){
+    //Create search URL
+    var searchUrl = "http://zippysharesearch.com/results.html?q=";
+    songName += " site:zippyshare.com"
+    //TODO: change the whitespace from what it is to whatever google prefers (maybe more search results)
+    console.log(parseSongNameWhitespace(songName));
+    searchUrl += parseSongNameWhitespace(songName);
+    //Get Results
+    var htmlString = "";
+    http.get(searchUrl, function(res) {
+      res.on('data', function(chunk) {
+        htmlString += chunk;
+        //console.log("chunk");
+        //console.log(chunk.toString());
+      });
+      res.on('end', function() {
+        // all data has been downloaded
+        //console.log("resolved zippysearch results:");
+
+        //console.log(htmlString);
+      });
+    });
+  }
+
+  function parseSongNameWhitespace(songName){
+    //Remove excess white space and change spaces to %20
+    return encodeURIComponent(songName.trim());
+  }
 
 /*GET library page. */
 router.get('/library/', ensureLoggedIn, function(req, res, next) {
