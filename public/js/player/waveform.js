e7e92b5c47fad5d3aa24566ca36f7b5b92f198da
Milad Imen <miladmaaan@gmail.com>
2016-08-28 16:46:18 -0700

    smoothed waveform animation

--- public/js/player/waveform.js
+++ public/js/player/waveform.js
@@ -113,15 +113,16 @@ function waveform() {
     })
     .attr("y", function(d, i) {
       var height = y(d * options.bar_height) / h;
-      if (i % 9 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
-      if (i % 9 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
-      if (i % 9 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
-      if (i % 9 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
-      if (i % 9 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
-      if (i % 9 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
-      if (i % 9 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
-      if (i % 9 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
-      if (i % 9 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
+      if (i % 10 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
+      if (i % 10 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
+      if (i % 10 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
+      if (i % 10 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
+      if (i % 10 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
+      if (i % 10 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
+      if (i % 10 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
+      if (i % 10 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
+      if (i % 10 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
+      if (i % 10 === 9) height *= composite(0, 2, 5, 6, 2, 0, 0); //15
       return h - Math.pow(Math.max(height, .01), 1.5);
     })
     .attr("width", function(d) {
@@ -130,15 +131,16 @@ function waveform() {
     })
     .attr("height", function(d, i) {
       var height = y(d * options.bar_height) / h;
-      if (i % 9 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
-      if (i % 9 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
-      if (i % 9 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
-      if (i % 9 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
-      if (i % 9 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
-      if (i % 9 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
-      if (i % 9 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
-      if (i % 9 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
-      if (i % 9 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
+      if (i % 10 === 0) height *= composite(1, 3, 4, 6, 1, 0, 0); //15
+      if (i % 10 === 1) height *= composite(2, 2, 7, 3, 1, 0, 0); //15
+      if (i % 10 === 2) height *= composite(4, 6, 3, 2, 0, 0, 0); //15
+      if (i % 10 === 3) height *= composite(8, 4, 2, 1, 0, 0, 0); //15
+      if (i % 10 === 4) height *= composite(10, 2, 1, 0, 0, 1, 1); //15
+      if (i % 10 === 5) height *= composite(8, 2, 0, 0, 0, 2, 3); //15
+      if (i % 10 === 6) height *= composite(3, 0, 0, 0, 1, 3, 8); //15
+      if (i % 10 === 7) height *= composite(2, 0, 0, 1, 2, 8, 2); //15
+      if (i % 10 === 8) height *= composite(0, 0, 1, 5, 7, 2, 0); //15
+      if (i % 10 === 9) height *= composite(0, 2, 5, 6, 2, 0, 0); //15
       return Math.pow(Math.max(height, .01), 1.5) + options.bar_y_offset;
     });
 }
